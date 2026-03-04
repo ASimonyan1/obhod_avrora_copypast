42096e9338d844abeae734e05abef25184ea2245
Alexanton <rabotantua@mail.ru>
2026-03-04 20:26:08 +0300

    Update main.py
    full functional 
    (CapsLock)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 import re
+from mmap import ACCESS_COPY
+
 from pynput.keyboard import Controller, Key
 
 print("Вставь код. Когда закончишь — напиши END на новой строке.\n")
@@ -20,6 +22,12 @@ CHAR_DELAY = 0.25
 LINE_DELAY = 1.5
 
 current_layout = "EN"  # предполагаем, что перед стартом включён английский
+current_cap = "OFF"  # предполагаем, что перед стартом Caps Lock выключен
+
+def toggle_cap():
+    keyboard.press(Key.shift)
+    keyboard.release(Key.shift)
+    time.sleep(0.1)
 
 def toggle_layout():
     keyboard.press(Key.alt_l)
@@ -31,6 +39,14 @@ def toggle_layout():
 def is_russian(ch):
     return re.match(r"[А-Яа-яЁё]", ch) is not None
 
+def is_rus_capital(ch):
+    return re.match(r"[А-ЯЁ]", ch) is not None
+
+def is_eng_capital(ch):
+    return re.match(r"[A-Z]", ch) is not None
+
+
+
 for line in lines:
     for ch in line:
 
@@ -40,6 +56,17 @@ for line in lines:
             toggle_layout()
             current_layout = want_layout
 
+        if current_cap == "RU":
+            want_cap = "ON" if is_rus_capital(ch) else "OFF"
+
+        else:
+            want_cap = "ON" if is_eng_capital(ch) else "OFF"
+
+        if want_cap != current_cap:
+            toggle_cap()
+            current_cap = want_cap
+
+        print(current_cap)
         keyboard.type(ch)
         time.sleep(CHAR_DELAY)
 
